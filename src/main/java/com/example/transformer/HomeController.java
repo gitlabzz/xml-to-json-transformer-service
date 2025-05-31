153b023cfd186dfec258e8155ef4d7da80936cdc
Asim Mirza <98458951+gitlabzz@users.noreply.github.com>
2025-05-31 17:45:38 +1000

    Add home page and OpenAPI support

---
+++ src/main/java/com/example/transformer/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.transformer;
+
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.info.GitProperties;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private final BuildProperties buildProperties;
+    private final GitProperties gitProperties;
+
+    public HomeController(BuildProperties buildProperties, GitProperties gitProperties) {
+        this.buildProperties = buildProperties;
+        this.gitProperties = gitProperties;
+    }
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String index(Model model) {
+        if (buildProperties != null) {
+            model.addAttribute("version", buildProperties.getVersion());
+            model.addAttribute("buildTime", buildProperties.getTime());
+        }
+        if (gitProperties != null) {
+            model.addAttribute("commitId", gitProperties.getShortCommitId());
+        }
+        return "index";
+    }
+}
